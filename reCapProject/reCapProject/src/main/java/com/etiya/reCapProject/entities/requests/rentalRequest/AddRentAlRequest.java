959980777445edd995f4be966c76542a21d27746
ibrahimdoss <72415820+ibrahimdoss@users.noreply.github.com>
2021-09-27 12:36:06 +0300

    work_12

---
+++ reCapProject/reCapProject/src/main/java/com/etiya/reCapProject/entities/requests/rentalRequest/AddRentAlRequest.java
@@ -0,0 +1,41 @@
+package com.etiya.reCapProject.entities.requests.rentalRequest;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.validation.annotation.Validated;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Validated
+
+public class AddRentAlRequest {
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private String rentDate;
+
+	private String returnDate;
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private int carId;
+	
+	@NotNull(message = "Boş Geçilemez!")
+	@NotBlank
+	private String carName;
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private int customerId;
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private boolean saveCreditCard;
+
+}
---
+++ reCapProject/reCapProject/src/main/java/com/etiya/reCapProject/entities/requests/rentalRequest/UpdateRentAlRequest.java
@@ -0,0 +1,40 @@
+package com.etiya.reCapProject.entities.requests.rentalRequest;
+
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateRentAlRequest {
+	
+	@NotNull(message = "Boş Geçilemez!")
+	
+	private int rentAlId;
+
+	
+	
+	@NotNull(message = "Boş Geçilemez!")
+	
+	private String carName;
+	
+	private String rentDate;
+	
+	
+	private String returnDate;
+	
+	
+	
+	@NotNull(message = "Boş Geçilemez!")	
+	private int carId;
+	
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private int customerId;
+}
